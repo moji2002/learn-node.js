4be36b0023e0381f47d5a06c4a5954f2a69a0425
Mojtaba <moji2002@gmail.com>
2020-12-14 23:10:56 +0330

    new folders

---
+++ example/joi.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const Joi = require('joi');
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+const courses = [
+    { id: 1, name: "course-1" },
+    { id: 2, name: "course-2" },
+    { id: 3, name: "course-3" },
+    { id: 4, name: "course-4" },
+];
+
+// validation with joi
+app.post('/api/courses', (req, res) => {
+    // for start with joi first we need to define a schema ,it defines the shape of object
+    const courseSchema = Joi.object({
+        name: Joi.string().min(3).required(),
+        author: Joi.string().min(3).required()
+    });
+
+    // we can validate the body with this method
+    const validationResult = courseSchema.validate(req.body);
+    if (validationResult.error) return res.status(400).send(validationResult.error.details[0].message);
+
+    const newCourse = {
+        id: courses.length + 1,
+        name: req.body.name,
+        author: req.body.author
+    };
+
+    courses.push(newCourse);
+    res.send({ newCourse, length: courses.length });
+});
+
+app.put('/api/courses/:id', (req, res) => {
+    // Look up the course with the given id
+    // If not existing, return 404 ,means not found
+    const course = courses.find(course => course.id === parseInt(req.params.id));
+    if (!course)
+        return res.status(404).send('The course with the given ID was not found!');
+
+    // If founded we should validate
+    // If invalid we should return 400 ,means Bad request
+    const courseSchema = Joi.object({
+        name: Joi.string().min(3).required(),
+        author: Joi.string().min(3).required()
+    });
+
+    const validationResult = courseSchema.validate(req.body);
+    if (validationResult.error)
+        return res.status(400).send(validationResult.error.details[0].message);
+
+    // If it is valid we should update the course 
+    course.name = req.body.name;
+    course.author = req.body.author;
+    
+    // Then return updated course to client
+    res.send(course);
+});
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
---
+++ example/start.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+
+//get request
+const courses = [
+    { id: 1, name: "course-1" },
+    { id: 2, name: "course-2" },
+    { id: 3, name: "course-3" },
+    { id: 4, name: "course-4" },
+];
+
+app.get('/api/courses', (req, res) => {
+    res.send("courses");
+});
+
+//parameters
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(course => course.id === parseInt(req.params.id));
+    if (!course) res.status(404)
+        .send('The course with the given ID was not found!');
+    res.send(course);
+});
+
+//multiple parameters  => for require values
+app.get('/blog/:year/:month', (req, res) => {
+    res.send(req.params);
+});
+
+//query data parameters  => for optional values => for example => http://localhost:3000/query?sort=name
+app.get('/query', (req, res) => {
+    res.send(req.query);
+});
+
+
+// post request 
+// by convention when we post an object to the server,
+// when the server create new resourse we should return that object in the body of response
+// maybe the client needs to know the id of new object or new resourse
+
+// we should post somthing like this in the body of request
+//    {"name":"new course"}
+//
+// As a security best practice  you should never trust what the client send you , you should always validate the input
+// simple validation 
+app.post('/api/courses', (req, res) => {
+    if (!req.body.name)
+        return res.status(400).send('Name is reqired.');
+    if (req.body.name.length < 3)
+        return res.status(400).send('Name should be minimum 3 characters.');
+
+    const newCourse = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+
+    courses.push(newCourse);
+    res.send({ newCourse, length: courses.length });
+});
+
+
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
