5d2a236bd121e57acc67ecfe226cf1340824ec02
mojtaba beheshti <moji2002@gmail.com>
2020-12-23 15:31:18 +0330

    add /me route

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const auth = require('../middlewares/auth');
 const router = express.Router();
+
+router.get("/me", auth, async (req, res) => {
+    try {
+      const { firstName, lastName, email } = await User.findById(req.user.id);
+      return res.send({ firstName, lastName, email });
+    } catch (error) {
+      return res.status(400).send({ error, msg: error.message });
+    }
+  });
 
 router.get('/', (req, res) => {
     res.send('hello world');
