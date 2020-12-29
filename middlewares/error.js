817ecb063ad806c7ec6b788c98a7fcb1a50f7ce0
Mojtaba <moji2002@gmail.com>
2020-12-29 23:41:34 +0330

    error middleware

---
+++ middlewares/error.js
@@ -0,0 +1,5 @@
+const error = (error, req, res, next) => {
+    res.status(400).send({ message: error.message });
+};
+
+module.exports = error;
--- routes/upload.js
+++ routes/upload.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const error = require('../middlewares/error');
 const router = express.Router();
-const uploadImage = require('../middlewares/multer')
+const uploadImage = require('../middlewares/multer');
 
 router.post('/', uploadImage.single('upload'), (req, res) => {
     res.send(req.body);
-});
+}, error);
 
 module.exports = router;
